b35c945756817a04ed8c8f939110fb75d4b485d3
piro[studio] <piotr@pirostudio.pl>
2017-11-01 15:21:43 +0100

    Create display-category-without-link-list.php

---
+++ display-category-without-link-list.php
@@ -0,0 +1,15 @@
+// display category name + link (without list)
+
+foreach((get_the_category()) as $category) { 
+   echo '<a href="'; 
+   echo get_category_link($category->cat_ID); 
+   echo '">'; 
+   echo $category->cat_name . ' '; 
+   echo '</a>'; 
+}
+
+// display category name
+
+foreach((get_the_category()) as $category) { 
+   echo $category->cat_name . ' '; 
+}
