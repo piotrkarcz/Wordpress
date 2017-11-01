fdd42e3bd619ae26355fdf0d255438bd9a6cc1a2
piro[studio] <piotr@pirostudio.pl>
2017-11-01 15:36:43 +0100

    Update and rename wp-admin access only for administrator to wp-admin-access-only-for-administrator.php

--- wp-admin-access-only-for-administrator.php
+++ wp-admin-access-only-for-administrator.php
@@ -1,6 +1,10 @@
+<?php
+
 function wp_restrict_admin() {
     if ( is_admin() && ! current_user_can( 'administrator' ) && ! ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) {
         wp_die( __('Only for admin') );
     }
 }
 add_action( 'admin_init', 'wp_restrict_admin', 1 );
+
+?>
